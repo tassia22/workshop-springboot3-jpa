dcb8d9c1758dab4d91a5c77a5999353658629d4a
Tássia Pereira <tassiapereira2020@gmail.com>
2024-05-16 23:34:19 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -69,7 +69,7 @@ public class TestConfig implements CommandLineRunner{
 		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "234356");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "234356																																																																																																																																																																																																																																																																																																																																																																																																																																																																										");
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
@@ -78,7 +78,7 @@ public class TestConfig implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
-		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());																																																																																																																																																																					
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
